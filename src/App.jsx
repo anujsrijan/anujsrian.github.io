20c502439dcd92706acd0e08fae480fa2d7adb53
anujsrijan <anuj.srijan@gmail.com>
2025-07-25 20:09:11 +0530

    updated index.html

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/home";
 import NavBar from "../src/components/ui/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
       <main className="container">
